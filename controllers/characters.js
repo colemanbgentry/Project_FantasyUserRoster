74589d4681339138aa5f7ecbff1a6d4d12cfe472
Coleman B Gentry <104287033+colemanbgentry@users.noreply.github.com>
2023-12-06 22:29:12 -0800

    Fixing health

--- controllers/characters.js
+++ controllers/characters.js
@@ -52,7 +52,7 @@ const updateCharacter = async (req, res) => {
         characterName: req.body.characterName,
         class: req.body.class,
         level: req.body.level,
-        health: req.body.heatlh,
+        health: req.body.health,
         magicPoints: req.body.magicPoints,
         strength: req.body.strength,
         agility: req.body.agility,
